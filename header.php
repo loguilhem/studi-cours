85e30019f4489e64aa291ce9898c84bc04a02c6d
guilhem <guilhem@alteragile.com>
2020-12-01 19:29:43 +0100

    first

---
+++ header.php
@@ -0,0 +1,19 @@
+<?php
+session_start ();
+
+?>
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+        <title>Design patterns</title>
+    </head>
+    <body>
+        <div class="container">
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+ini_set('display_errors', 1);
+
+include_once 'header.php';
+
+
+?>
+
+    <h1>Les design patterns</h1>
+
+    <?php
+
+
+    ?>
+
+<?php
+    include_once "footer.php";
+?>
