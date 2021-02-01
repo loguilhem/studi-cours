8c8de28fe52afd8682c5d232cf689b752ebf0ed6
guilhem <guilhem@alteragile.com>
2021-02-01 19:17:38 +0100

    ok pour cours

--- index.php
+++ index.php
@@ -1,35 +1,79 @@
 <?php
-declare(strict_types = 1);
 error_reporting(E_ALL);
 ini_set('display_errors', 'on');
-/* ici si on met 1 à la place 'on', ca pète en strict type car la fonction attend un string */
 
-// 1 - Créer des variables et les afficher
-// 2 - Modifier la valeur et l'afficher
-// 3 - Créer des constantes et les afficher
-// 4 - Tenter de modifier la valeur de la constante
-// 5 - Les différents type de variable : int, float, string, array, bool
-// 6 - créer un int et l'afficher
-// 7 - créer un float et l'afficher
-// 8 - multiplier float par int
-// 9 - remplacer le float en string et montrer que ca marche car PHP n'est pas fortement typé
-// 10 - mettre declare(strict_types = 1); en haut du fichier
-// 11 - erreur si 1 à la place de 'on' dans init_st, créer la fonction echo price (float-string)
-// 12 - caster le string price en float puis int et montrer avec des var_dump
-// 13 - créer un boolean et l'utiliser dans une condition
-// 13 - montrer les incrémentations
+/**
+ * 1 - Créer un tableau avec array et []
+ * 2 - Montrer que l'on peut y mettre ce qu'on veut
+ * 3 - Montrer comment parcourir un tableau
+ * 4 - Montrer que les clés sont autogénérees
+ * 5 - Montrer comment générés des clés, cf les artistes
+ * 6 - Montrer comment modifier une valeur grâce à sa clé
+ * 7 - Montrer comment ajouter une valeur via la clé ou via array push
+ * 8 - Montrer array merge, cf les fruits
+ * 9 - Montrer in_array
+ * 10 - Montrer array-column cf les artistes
+ * 11 - Montrer implode avec firstnames
+ * 12 - Montrer explode avec une liste de sport
+ * 13 - Montrer array unique, asort sur sports
+ * 14 - Montrer le système de référence : @see https://blog.penjee.com/wp-content/uploads/2015/02/pass-by-reference-vs-pass-by-value-animation.gif
+ *
+ */
 
-$lastName = 'Anderson';
-const FIRSTNAME = 'Rita';
+$myArray = [];
+$myArrayJr = array();
 
-$nbChildren = 6;
-$nbDiscSold = 200000000;
-$unitPriceDisc = '12.60';
+$nawak = [
+    'string',
+    1,
+    1.05,
+    true,
+    [
+        100,
+        'string2',
+        'coucou'
+    ]
+];
 
-//var_dump($unitPriceDisc);
-//var_dump((int)$unitPriceDisc);
+$nawak[0] = 'ma super cool string';
 
-$fortune = $nbDiscSold/$unitPriceDisc;
+$artists = [
+    [
+        'firstName' => 'Bob',
+        'lastName' => 'Marley',
+        'style' => 'Reggae'
+    ],
+    [
+        'firstName' => 'Freddy',
+        'lastName' => 'Mercury',
+        'style' => 'Rock'
+    ],
+    [
+        'firstName' => 'Robert',
+        'lastName' => 'Clegg',
+        'style' => 'Pop Zulu'
+    ],
+];
+
+$firstnames = array_column($artists, 'firstName');
+$firstnamesString = implode('-', $firstnames);
+
+$artists[2]['firstName'] = 'Johnny';
+
+$cindy = ['firstName' =>  'Cindy', 'lastName' => 'Lauper', 'style' => 'Pop'];
+//$artists[3] = $cindy;
+array_push($artists, $cindy);
+
+$fruits = ['Banana', 'Tomato', 'Pineapple'];
+$vegetables = ['Avocado', 'Lettuce', 'Onion'];
+
+if (!in_array('Pepper', $vegetables)) {
+    array_push($vegetables, 'Pepper');
+}
+
+$sports = 'football-rugby-basketball-handball-waterpolo-football';
+$sportArray = explode('-',$sports);
+asort($sportArray, SORT_REGULAR);
 
 ?>
 
@@ -43,54 +87,59 @@ $fortune = $nbDiscSold/$unitPriceDisc;
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12">
-                    <h1>Les variables et les constantes</h1>
-                    <p>Avant de d'épouser Bob, le nom de jeune fille de <?php echo FIRSTNAME; ?> était : <?php echo $lastName; ?></p>
-                    <?php $lastName = 'Marley'; ?>
-                    <p>Puis elle pris le nom de <?php echo $lastName; ?></p>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-6">
-                    <?php
-                        echo 'ensemble, ils on eu '.$nbChildren.' enfants';
-                        echo '<br>';
-                        echo 'Bob a vendu '.$nbDiscSold.' albums';
-                        echo '<br>';
-                        echo 'Il a donc amassé '.$fortune.' $';
-                    echo '<br>';
-                    echo '<br>';
-                    echo '<br>';
+                    <h1>Les tableaux</h1>
+                    <table class="table table-bordered table-striped">
+                        <?php
+                        foreach ($nawak as $key => $item) {
+                            if (is_array($item)) {
+                                foreach ($item as $k => $i) {
+                                    echo '<tr><td>'.$k.'</td><td>'.$i.'</td></tr>';
+                                }
+                            } else {
+                                echo '<tr><td>'.$key.'</td><td>'.$item.'</td></tr>';
 
-                        $rich = true;
-                        var_dump($rich);
-                        if (!$rich) {
-                            echo 'Bob était riche ';
-                        } else {
-                            echo 'Bob était pauvre';
+                            }
                         }
-
-                        $ourFortune = 1;
-//                        for ($i = 0; $i < 50; $i++) {
-//                            echo $i.' ';
-//                            //$ourFortune += 1; //$ourFortune++;
-////                            $ourFortune = $ourFortune*2;
-//                            $ourFortune += $ourFortune;
-//                            echo $ourFortune;
-//                            echo '<br>';
-//                        }
-
-                        // Pour montrer l'importance du typage
-                        function printPrice(float $price) {
-                            echo 'prix du disc : '. $price.' €';
+                        ?>
+                    </table>
+                    <h2>Les artistes</h2>
+                    <table class="table table-bordered table-striped">
+                        <?php
+                        foreach ($artists as $artist) {
+                            echo '<tr>';
+                            foreach ($artist as $key => $value) {
+                                echo '<td>'.$key.'</td><td>'.$value.'</td>';
+                            }
+                            echo '</tr>';
                         }
-                        echo printPrice($unitPriceDisc);
+                        ?>
+                    </table>
+                    <p>
+                        Les prénoms des artistes sont : <?php echo $firstnamesString; ?>
+                    </p>
+                    <h2>Les courses</h2>
+                    <table class="table table-bordered table-striped">
+                        <?php
+                            foreach (array_merge($fruits, $vegetables) as $item) {
+                                echo $item.'<br>';
+                            }
+                        ?>
+                    </table>
+                    <h3>Les sports</h3>
+                    <p>
+                        <?php
+                            foreach ($sportArray as $key => $sport) {
+                                $sportArray[$key] = 'toto';
 
-                    ?>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-6">
-
+                                echo $sport;
+                                echo '<br>';
+                            }
+                        foreach ($sportArray as $key => $sport) {
+                            echo $sport;
+                            echo '<br>';
+                        }
+                        ?>
+                    </p>
                 </div>
             </div>
         </div>
